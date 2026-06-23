dca2162159f6fee393a010f900ab1c2896d4e004
Manzilla Moktan <146763312+mmanzilla@users.noreply.github.com>
2026-06-23 15:25:39 -0500

    Configure Mochawesome reporter for Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports/individual',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
     setupNodeEvents(on, config) {},
   },
 });
