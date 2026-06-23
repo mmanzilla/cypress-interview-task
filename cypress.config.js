59c8359edd06b97a6f75e1a238a13ce744228441
Manzilla Moktan <146763312+mmanzilla@users.noreply.github.com>
2026-06-23 16:16:04 -0500

    Add projectId to Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    projectId: 'ryogfp',
     supportFile: false,
     reporter: 'mochawesome',
     reporterOptions: {
