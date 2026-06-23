6e5d9a3ff740cad05c1ae7cbe29d36e5e320ef36
Manzilla Moktan <146763312+mmanzilla@users.noreply.github.com>
2026-06-23 15:54:15 -0500

    Add checkbox verification tasks to Cypress test

--- cypress/e2e/task.cy.js
+++ cypress/e2e/task.cy.js
@@ -1,6 +1,11 @@
 describe('Checkbox Task', () => {
   it('should verify checkboxes', () => {
     cy.visit('https://the-internet.herokuapp.com/checkboxes');
-
+    
+    // Task 2: Verify there are 2 checkboxes
+    cy.get('input[type="checkbox"]').should('have.length', 2);
+    
+    // Task 3: Check the first checkbox and verify it is checked
+    cy.get('input[type="checkbox"]').first().check().should('be.checked');
   });
 });
